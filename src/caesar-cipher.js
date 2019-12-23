74e14e4e414c1ac8b5f1161ca294c9b3c9004aee
AndresFMoya <andresfmoya@gmail.com>
2019-12-23 16:03:42 -0500

    style: fix linter errors

--- src/caesar-cipher.js
+++ src/caesar-cipher.js
@@ -1,30 +1,31 @@
-const caesar = (str, offset) => {
-    let res = [];
-    offset %= 25; // Allow large outputs
-    str.split('').forEach(c => {
-        let code = ascii(c);
-        if(isLower(code)) {
-            if((code + offset) > ascii('z')) {
-                code = ascii('a') + (code + offset - ascii('z') - 1);
-            } else {
-                code += offset;
-            }
-        }
-        if(isUpper(code)) {
-            if((code + offset) > ascii('Z')) {
-                code = ascii('A') + (code + offset - ascii('Z') - 1);
-            } else {
-                code += offset;
-            }
-        }
-        res.push(String.fromCharCode(code));
-    });
-    return res.join('');
-};
-
+const ascii = char => char.charCodeAt(0);
 const isLower = code => code >= ascii('a') && code <= ascii('z');
 const isUpper = code => code >= ascii('A') && code <= ascii('Z');
-const ascii = char => char.charCodeAt(0);
+
+const caesar = (str, off) => {
+  const res = [];
+  let offset = off;
+  offset %= 25; // Allow large outputs
+  str.split('').forEach(c => {
+    let code = ascii(c);
+    if (isLower(code)) {
+      if ((code + offset) > ascii('z')) {
+        code = ascii('a') + (code + offset - ascii('z') - 1);
+      } else {
+        code += offset;
+      }
+    }
+    if (isUpper(code)) {
+      if ((code + offset) > ascii('Z')) {
+        code = ascii('A') + (code + offset - ascii('Z') - 1);
+      } else {
+        code += offset;
+      }
+    }
+    res.push(String.fromCharCode(code));
+  });
+  return res.join('');
+};
 
 caesar('abc', 2);
 
