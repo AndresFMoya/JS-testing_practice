dd7ec23be421da55d41440b76db309fe49355b82
AndresFMoya <andresfmoya@gmail.com>
2020-01-17 10:41:19 -0500

    test: testing sticler error

--- test/array-analysis.test.js
+++ test/array-analysis.test.js
@@ -6,3 +6,5 @@ it('average', () => expect(arrayAnalysis(array).average).toBe(4));
 it('min', () => expect(arrayAnalysis(array).min).toBe(1));
 it('max', () => expect(arrayAnalysis(array).max).toBe(7));
 it('length', () => expect(arrayAnalysis(array).length).toBe(7));
+
+
